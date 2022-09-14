9cfc31db0e62a299c72e85b2a197a9b5227b3e70
chenyong@qizuang.com <chenyong@qizuang.com>
2022-09-14 22:03:17 +0800

    first commit

---
+++ CYNetworking/Classes/CYNetworkingManager.h
@@ -0,0 +1,16 @@
+//
+//  CYNetworkingManager.h
+//  ModuleB
+//
+//  Created by chenyon on 2022/9/14.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CYNetworkingManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CYNetworking/Classes/CYNetworkingManager.m
@@ -0,0 +1,12 @@
+//
+//  CYNetworkingManager.m
+//  ModuleB
+//
+//  Created by chenyon on 2022/9/14.
+//
+
+#import "CYNetworkingManager.h"
+
+@implementation CYNetworkingManager
+
+@end
